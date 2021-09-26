7e453b1efd0bae40c6529dab8a81635221c79618
Antoine Meilliez <meilliez.antoine@gmail.com>
2021-09-26 22:01:15 +0200

    Add wikipedia recipe case

---
+++ lambda_scraper/scraping_recipes/recipes/wikipedia.py
@@ -0,0 +1,22 @@
+from .recipe import BSRecipe
+import requests
+
+class WikipediaRecipe(BSRecipe):
+
+    def __init__(self, activated:bool=True):
+        self.name = 'Wikipedia Recipe'
+        self.activated = activated
+        self.entrypoints = ["Assurance", "Aéronautique", "Construction_automobile", "Banque"]
+        self.base_url = "https://fr.wikipedia.org/wiki/"
+    
+    def run_url_parsing(self):
+        #TODO
+        return super().run_url_parsing()
+    
+    def run_scraping(self):
+        #TODO
+        return super().run_scraping()
+    
+    def run_file_making(self):
+        #TODO
+        return super().run_file_making()
