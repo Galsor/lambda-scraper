8eea57eae73b9ff736bc88168645f56894c11bf5
Antoine Meilliez <meilliez.antoine@gmail.com>
2021-09-26 13:42:46 +0200

    isort

--- lambda_scraper/drivers/selenium_chrome.py
+++ lambda_scraper/drivers/selenium_chrome.py
@@ -1,17 +1,14 @@
-from selenium import webdriver
 from omegaconf import DictConfig
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 from .driver import Driver
 
 
 class ChromeDriver(Driver, webdriver.Chrome):
-
     def __init__(self, cfg: DictConfig):
         self.options = ChromeOptions()
         for key, value in cfg.driver.items():
             if value == True:
                 self.options.add_argument(f"--{key}")
         super().__init__(options=self.options)
-
-
