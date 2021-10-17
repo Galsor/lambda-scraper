e82d48e26dcc8e08991918754baa28be2042b9d7
MeillieA <meilliez.antoine@gmail.com>
2021-10-17 21:56:40 +0200

    Fix plugins path access

--- lambda_scraper/recipe/__init__.py
+++ lambda_scraper/recipe/__init__.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from importlib import import_module
 
-plugin_dir = Path("./plugins/")
+plugin_dir = (Path(__file__).parent / "plugins").resolve()
 assert plugin_dir.is_dir()
 
 PLUGINS = []
 
 for file in plugin_dir.glob("*.py"):
-    PLUGINS.append(file.name)
-    import_module(file.name)
+    if file.name != "__init__.py":
+        PLUGINS.append(file.name)
+        import_module(file.name)
