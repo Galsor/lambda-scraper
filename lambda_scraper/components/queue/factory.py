b981b29a50606d48d3db7cce55862f0f48d62bf0
MeillieA <meilliez.antoine@gmail.com>
2021-10-17 14:53:56 +0200

    Reformat code

--- lambda_scraper/components/queue/factory.py
+++ lambda_scraper/components/queue/factory.py
@@ -1,10 +1,13 @@
 from lambda_scraper.components.factory import Factory
 
+
 class QueueFactoryClass(Factory):
 
     def register(self, doc=None):
         def fn(cls):
             return super(QueueFactoryClass, self).register(cls.__name__, doc)(cls)
+
         return fn
+
 
 QueueFactory = QueueFactoryClass('Queue')
--- lambda_scraper/components/queue/local.py
+++ lambda_scraper/components/queue/local.py
@@ -3,6 +3,7 @@ from queue import SimpleQueue
 from .factory import QueueFactory
 from lambda_scraper.components.queue.ls_queue import LambdaScraperQueue
 
+
 @QueueFactory.register('Simple Queue enabling local use of the scraper')
 class LocalQueue(SimpleQueue, LambdaScraperQueue):
     """Minimal Queue for testing"""
