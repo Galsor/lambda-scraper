fddd853415646a628e7439e595e45bd8bcaefb44
MeillieA <meilliez.antoine@gmail.com>
2021-10-17 22:45:47 +0200

    Fix factory

--- lambda_scraper/components/queue/factory.py
+++ lambda_scraper/components/queue/factory.py
@@ -4,8 +4,8 @@ from lambda_scraper.components.factory import Factory
 class QueueFactoryClass(Factory):
 
     def register(self, doc=None):
-        def fn(cls):
-            return super(QueueFactoryClass, self).register(cls.__name__, doc)(cls)
+        def fn(cls, *args, **kwargs):
+            return super(QueueFactoryClass, self).register(cls.__name__, doc)(cls, *args, **kwargs)
 
         return fn
 
--- lambda_scraper/components/queue/local.py
+++ lambda_scraper/components/queue/local.py
@@ -4,7 +4,7 @@ from .factory import QueueFactory
 from lambda_scraper.components.queue.ls_queue import LambdaScraperQueue
 
 
-@QueueFactory.register('Simple Queue enabling local use of the scraper')
+@QueueFactory.register(doc='Simple Queue enabling local use of the scraper')
 class LocalQueue(SimpleQueue, LambdaScraperQueue):
     """Minimal Queue for testing"""
 
