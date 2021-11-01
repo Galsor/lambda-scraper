d174cd7784863223480730dcd14910239a3d4cf7
Antoine Meilliez <meilliez.antoine@gmail.com>
2021-11-01 20:39:33 +0100

    Refactor plugin load

--- lambda_scraper/recipe/__init__.py
+++ lambda_scraper/recipe/__init__.py
@@ -1,14 +1,24 @@
 from importlib import import_module
 from pathlib import Path
+from enum import Enum
+import logging
+from lambda_scraper.utils.types import BaseTask
+
+logger = logging.getLogger("Initialize plugins")
 
 plugin_dir = (Path(__file__).parent / "plugins").resolve()
 assert plugin_dir.is_dir()
+
+import_module(f"lambda_scraper.recipe.plugins")
 
 PLUGINS = []
 
 for file in plugin_dir.glob("*.py"):
     if file.name != "__init__.py":
-        PLUGINS.append(file.name)
+        # ex: ("wikipedia", <class wikipedia>)
+        PLUGINS.append((file.stem, type(__name__, (BaseTask,), {})))
 
-# FIXME Check if modules are really imported.
-import_module("lambda_scraper.recipe.plugins")
+# List all available scraping plugins
+Tasks = Enum("Task", PLUGINS)  # Auto enum with plugins names
+
+logger.debug(f"Available plugins: {[e.name for e in Tasks]}")
---
+++ lambda_scraper/utils/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+ROOT_DIR = Path(__file__).parents[2].resolve()
+SRC_DIR = Path(__file__).parents[1].resolve()
+DATA_DIR = ROOT_DIR / "data"
+CONFIG_DIR = ROOT_DIR / "config"
